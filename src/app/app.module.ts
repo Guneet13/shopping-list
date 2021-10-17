151befc92985f9db728707f913c974140fb7f357
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-17 13:56:04 -0700

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingComponent } from './shopping/shopping.component';
 
@@ -12,7 +11,6 @@ import { ShoppingComponent } from './shopping/shopping.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
